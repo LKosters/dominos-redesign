71332ba511b72521899e7a984a0fd9f657df012d
LKosters <lau.kosters@gmail.com>
2024-09-30 10:13:37 +0200

    Added container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+    },
   },
   plugins: [],
 }
-
