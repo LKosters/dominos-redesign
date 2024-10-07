55ec155269eff032bf4fbd4b11b9432325c8337a
LKosters <main@laurenskosters.nl>
2024-10-07 13:06:32 +0200

    Changed USP's

--- components/ui/header/Topbar.vue
+++ components/ui/header/Topbar.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="bg-dominoBlue py-2">
-    <div class="container flex justify-around text-dominoWhite text-sm font-bold">
-      <p>USP 1</p>
-      <p>USP 2</p>
-      <p>USP 3</p>
+  <div class="bg-dominoBlue py-2 flex justify-center w-full">
+    <div class="container flex-col lg:flex-row flex justify-around text-dominoWhite text-sm font-bold w-full">
+      <p class="flex items-center w-full justify-center mb-3 lg:mb-0"><TruckIcon class="size-4 mr-3 stroke-2"/> Binnen 30 minuten in huis!</p>
+      <p class="flex items-center w-full justify-center mb-3 lg:mb-0"><GlobeEuropeAfricaIcon class="size-4 mr-3 stroke-2"/> Probeer onze nieuwe vegan opties!</p>
+      <p class="flex items-center w-full justify-center"><BuildingStorefrontIcon class="size-4 mr-3 stroke-2"/> Altijd een winkel in de buurt!</p>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { TruckIcon, GlobeEuropeAfricaIcon , BuildingStorefrontIcon } from '@heroicons/vue/24/outline';
+</script>
