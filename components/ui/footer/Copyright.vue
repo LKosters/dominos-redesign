d2870942291b61454d640d668df0ac18f7102c3e
LKosters <main@laurenskosters.nl>
2024-10-14 15:00:40 +0200

    Push

--- components/ui/footer/Copyright.vue
+++ components/ui/footer/Copyright.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <p class="text-center py-3 font-bold text-sm">
         Copyright 2024 Domino’s -
-        <NuxtLink class="duration-300 hover:text-dominoBlue" to="/">Algenemen voorwaarden</NuxtLink> -
+        <NuxtLink class="duration-300 hover:text-dominoBlue" to="/">Algemene voorwaarden</NuxtLink> -
         <NuxtLink class="duration-300 hover:text-dominoBlue" to="/">Privacy statement</NuxtLink>
       </p>
     </div>
